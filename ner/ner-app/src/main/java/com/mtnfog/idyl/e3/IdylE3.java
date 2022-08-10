d88edd20e6eef51536b5ee4e340d475bc9867aa8
jzonthemtn <jeff.zemerick@mtnfog.com>
2022-08-10 18:25:41 -0400

    Working toward redoing ner.

---
+++ ner/ner-app/src/main/java/com/mtnfog/idyl/e3/IdylE3.java
@@ -0,0 +1,163 @@
+/*******************************************************************************
+ * Copyright 2019 Mountain Fog, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.mtnfog.idyl.e3;
+
+import java.io.File;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.mtnfog.idyl.e3.metrics.MetricUtils;
+import com.mtnfog.idyl.e3.stats.ConsoleStatsReporter;
+import com.mtnfog.idyl.e3.stats.GraphiteStatsReporter;
+
+import ai.idylnlp.model.nlp.ConfidenceFilter;
+import ai.idylnlp.model.stats.StatsReporter;
+import ai.idylnlp.nlp.filters.confidence.HeuristicConfidenceFilter;
+import ai.idylnlp.nlp.filters.confidence.SimpleConfidenceFilter;
+import ai.idylnlp.nlp.filters.confidence.serializers.LocalConfidenceFilterSerializer;
+
+/**
+ * Runnable main application class for Idyl E3.
+ * 
+ * @author Mountain Fog, Inc.
+ *
+ */
+@SpringBootApplication(exclude = { 
+		MultipartAutoConfiguration.class,
+		JacksonAutoConfiguration.class, 
+		MongoAutoConfiguration.class,
+		MongoDataAutoConfiguration.class, 
+		SecurityAutoConfiguration.class
+})
+@Configuration
+@EnableScheduling
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class IdylE3 extends SpringBootServletInitializer {
+	
+	private static final Logger LOGGER = LogManager.getLogger(IdylE3.class);
+	
+	@Autowired
+	private Environment env;
+	
+	public static void main(String[] args) throws Exception {
+						
+		LOGGER.info("Starting Idyl E3.");
+		
+		SpringApplication.run(IdylE3.class, args);
+		
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+				
+		return application.sources(IdylE3.class);
+		
+	}
+	
+	@Bean
+	public StatsReporter getStatsReporter() {
+		
+		if(StringUtils.equalsIgnoreCase(env.getProperty("stats.graphite.enabled"), "true")) {
+			
+			final String host = env.getProperty("stats.graphite.host", "localhost");
+			final String port = env.getProperty("stats.graphite.port", "2003");
+			final String prefix = env.getProperty("stats.graphite.prefix", "ner");
+			final String interval = env.getProperty("stats.graphite.interval", "60");
+			
+			return new GraphiteStatsReporter(
+					host, 
+					Integer.valueOf(port), 
+					prefix, 
+					Long.valueOf(interval));
+			
+		} else {
+			
+			final long interval = Long.valueOf(env.getProperty("stats.console.interval", "300"));
+			
+			return new ConsoleStatsReporter(interval);
+			
+		}
+		
+	}
+	
+	@Bean
+	public ConfidenceFilter getConfidenceFilter() throws Exception {
+		
+		ConfidenceFilter filter = null;
+		
+		if(StringUtils.equalsIgnoreCase("confidence.heuristics", "true")) {
+			
+			LOGGER.info("Using heuristic confidence filtering.");
+				
+			double alpha = Double.valueOf(env.getProperty("confidence.heuristics.alpha", "0.05"));
+			int minSampleSize = Integer.valueOf(env.getProperty("confidence.heuristics.minSampleSize", "50"));
+			
+			final File directory = new File("data");
+			final File confidencesFile = new File(directory, env.getProperty("confidence.filename", "confidences.dat"));
+			
+			LOGGER.info("Confidence values will be serialized to {}", confidencesFile.getAbsolutePath());
+			
+			LocalConfidenceFilterSerializer serializer = new LocalConfidenceFilterSerializer(confidencesFile);
+
+			filter = new HeuristicConfidenceFilter(serializer, minSampleSize, alpha);			
+			
+		} else {
+			
+			LOGGER.info("Using simple confidence filtering.");
+			
+			filter = new SimpleConfidenceFilter();
+			
+		}
+		
+		filter.deserialize();
+		
+		return filter;
+		
+	}
+	
+	@Bean
+	public String getSystemId() {
+		
+		String systemId = env.getProperty("system.id");
+		
+		if(StringUtils.isEmpty(systemId)) {
+			
+			systemId = MetricUtils.getSystemId();
+			
+		}
+
+		return systemId;
+		
+	}
+
+}
---
+++ ner/ner-services/src/main/java/com/mtnfog/idyl/e3/services/DefaultInitializationService.java
@@ -0,0 +1,226 @@
+/*******************************************************************************
+ * Copyright 2019 Mountain Fog, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.mtnfog.idyl.e3.services;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mtnfog.idyl.e3.model.services.InitializationService;
+import com.mtnfog.idyl.e3.model.services.SystemInitializerService;
+import com.mtnfog.idyl.e3.utils.DefaultModelValidator;
+
+import ai.idylnlp.model.ModelValidator;
+import ai.idylnlp.model.manifest.ModelManifest;
+import ai.idylnlp.model.manifest.ModelManifestUtils;
+import ai.idylnlp.model.manifest.SecondGenModelManifest;
+import ai.idylnlp.model.manifest.StandardModelManifest;
+import ai.idylnlp.model.nlp.ConfidenceFilter;
+import ai.idylnlp.model.nlp.ner.EntityRecognizer;
+import ai.idylnlp.model.stats.StatsReporter;
+import ai.idylnlp.nlp.recognizer.DeepLearningEntityRecognizer;
+import ai.idylnlp.nlp.recognizer.OpenNLPEntityRecognizer;
+import ai.idylnlp.nlp.recognizer.RegularExpressionEntityRecognizer;
+import ai.idylnlp.nlp.recognizer.configuration.DeepLearningEntityRecognizerConfiguration;
+import ai.idylnlp.nlp.recognizer.configuration.OpenNLPEntityRecognizerConfiguration;
+import ai.idylnlp.opennlp.custom.modelloader.LocalModelLoader;
+import ai.idylnlp.opennlp.custom.modelloader.ModelLoader;
+import opennlp.tools.namefind.TokenNameFinderModel;
+
+/**
+ * Default implementation of {@link InitializationService} to configure
+ * Idyl E3's {@link IdylPipeline pipeline}.
+ * 
+ * @author Mountain Fog, Inc.
+ *
+ */
+@Component
+public class DefaultInitializationService implements InitializationService {
+
+	private static final Logger LOGGER = LogManager.getLogger(DefaultInitializationService.class);
+	
+	private List<EntityRecognizer> entityRecognizers;
+	private boolean loaded;
+	
+	@Autowired
+	private ConfidenceFilter confidenceFilter;
+
+	@Autowired
+	private SystemInitializerService initializerService;
+	
+	@Override
+	public boolean isLoaded() {		
+		return loaded;
+	}
+	
+	@Override
+	public List<EntityRecognizer> getEntityRecognizers() {
+		return entityRecognizers;
+	}
+	
+	@Override
+	@PostConstruct
+	public void initialize() {
+		
+		LOGGER.info("Initializing Idyl E3.");
+		
+		initializerService.initialize();
+		
+		entityRecognizers = new LinkedList<EntityRecognizer>();
+		loaded = false;
+		
+		final ModelValidator validator = new DefaultModelValidator();
+		
+		try {
+
+			final String modelsDirectory = new File("models").getAbsolutePath();
+	
+			LOGGER.info("Using model directory: " + modelsDirectory);
+			
+			// Create the model loaders.
+			ModelLoader<TokenNameFinderModel> entityModelLoader = new LocalModelLoader<TokenNameFinderModel>(validator, modelsDirectory);
+			
+			// Create the configuration given the modelLoader and the models.	
+			OpenNLPEntityRecognizerConfiguration modelEntityRecognizerConfiguration = new OpenNLPEntityRecognizerConfiguration.Builder()
+				.withEntityModelLoader(entityModelLoader)
+				.withConfidenceFilter(confidenceFilter)
+				.build();
+			
+			modelEntityRecognizerConfiguration.setStatsReporter(statsReporter);
+			
+			// Create the Deep Learning configuration.
+			DeepLearningEntityRecognizerConfiguration deepLearningEntityRecognizerConfiguration = 
+					new DeepLearningEntityRecognizerConfiguration.Builder()
+					.build(modelsDirectory);
+			
+			deepLearningEntityRecognizerConfiguration.setConfidenceFilter(confidenceFilter);
+			deepLearningEntityRecognizerConfiguration.setStatsReporter(statsReporter);			
+						
+			// Get the available model manifests.
+			List<ModelManifest> modelManifests = ModelManifestUtils.getModelManifests(modelsDirectory);
+						
+			if(CollectionUtils.isNotEmpty(modelManifests)) {
+			
+				for(ModelManifest modelManifest : modelManifests) {										
+										
+					if(modelManifest.getGeneration() == ModelManifest.FIRST_GENERATION) {
+					
+						StandardModelManifest standardModelManifest = (StandardModelManifest) modelManifest;
+													
+						// Load the token name finder model (entity model).
+						
+					    LOGGER.info("Entity Class: {}, Model File Name: {}, Language Code: {}",
+					    		modelManifest.getType(), modelManifest.getModelFileName(), modelManifest.getLanguageCode()); 		    			  
+					    
+				    	LOGGER.info("Enabling model {}.", modelManifest.getModelFileName());		
+				    	
+						// Add the entity model to the configuration.
+						modelEntityRecognizerConfiguration.addEntityModel(modelManifest.getType(), modelManifest.getLanguageCode(), standardModelManifest);						
+					
+					} else {
+						
+						// Handle loading a second generation model.
+
+						SecondGenModelManifest secondGenModelManifest = (SecondGenModelManifest) modelManifest;
+						
+						boolean valid = validator.validate(modelManifest);
+
+						if(valid) {
+						
+							LOGGER.info("Loading second generation model for type {} and language {}", modelManifest.getType(), modelManifest.getLanguageCode().getAlpha3().toString());
+							deepLearningEntityRecognizerConfiguration.addEntityModel(modelManifest.getType(), modelManifest.getLanguageCode(), secondGenModelManifest);
+							
+						} else {
+							
+							LOGGER.warn("License validation for the model {} failed. Model will not be loaded.", secondGenModelManifest.getModelFileName());
+							
+						}
+						
+					}
+					
+				}
+				
+			} else {
+				
+				LOGGER.warn("No model manifest files were found.");
+				
+			}
+														
+			// Load the models in a separate thread to not block startup.
+			final Thread modelLoadingThread = new Thread() {
+				
+			    @Override
+				public void run() {
+			    			    		
+			    	// Create the entity recognizers.
+			    	EntityRecognizer opennlpEntityRecognizer = new OpenNLPEntityRecognizer(modelEntityRecognizerConfiguration);
+			    	EntityRecognizer deepLearningEntityRecognizer = new DeepLearningEntityRecognizer(deepLearningEntityRecognizerConfiguration);
+			    	
+			    	// Add the entity recognizers to the list.
+			    	entityRecognizers.add(opennlpEntityRecognizer);
+			    	entityRecognizers.add(deepLearningEntityRecognizer);		 
+
+			    	// Initialize pattern-based entity recognizers.
+			    	try {
+						final File patternsFile = new File("patterns.properties");
+						if(patternsFile.exists()) {
+							final List<String> lines = IOUtils.readLines(new FileInputStream(patternsFile));
+							for(String line : lines) {
+								// Expected format is: entitytype=regex
+								final String entityType = line.substring(0, line.indexOf("="));
+								final Pattern pattern = Pattern.compile(line.substring(entityType.indexOf("=") + 1));
+								entityRecognizers.add(new RegularExpressionEntityRecognizer(pattern, entityType));
+							}
+							
+						} else {
+							LOGGER.info("Skipping pattern entity recognizers because no patterns.properties file found.");
+						}
+			    	} catch (Exception ex) {
+			    		LOGGER.error("Unable to initialize pattern entity recognizers.", ex);
+			    	}
+
+			    	loaded = true;
+			    	
+			    }				
+			
+			};
+
+			// Start the thread to load the models.
+			modelLoadingThread.start();
+			
+			// All done.
+			LOGGER.info("Idyl E3 API is ready. Models may still be loading.");
+			
+		} catch (Exception ex) {
+
+			LOGGER.error("Unable to initialize Idyl E3.", ex);
+
+		}
+				
+	}
+			
+}
